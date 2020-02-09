b968cc42c883e7b167eb046909926c4956233dfb
Nishil <nishilkapadia17@gmail.com>
2020-02-09 11:51:13 +0530

    added sumzero algo

--- Patterns/multiplepointer.js
+++ Patterns/multiplepointer.js
@@ -5,3 +5,27 @@
 //SumZero
 //Write a function called sumZero which accepts a sorted array of integers. The function shoudl find the firts pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
 
+
+//Examples: sumZero([-3,-2,-1,0,1,4,5]); //  [-1,1];
+
+// Colt Approach 
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right){
+        let sum = arr[right] + arr[left];
+        if(sum === 0){
+            return [arr[left],arr[right]]; 
+        }else if(sum > 0 ){
+            right--;
+        }else{
+            left++;
+        }
+    }
+}
+
+// Time complexity : O(n);
+
+
+
+
