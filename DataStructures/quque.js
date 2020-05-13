0224c83cc3aba49443cb1c0e15b9f7b397e4a2a9
Nishil <nishilkapadia17@gmail.com>
2020-05-13 13:45:57 +0530

    ququq modified

--- DataStructures/quque.js
+++ DataStructures/quque.js
@@ -1,61 +1,54 @@
 //queue
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
+class queue {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.len = 0;
+	}
 
-class queue{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.len = 0;
-    }
+	enqueue(val) {
+		let newNode = new Node(val);
 
-    enqueue(val){
-        let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
 
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-        this.tail.next = newNode;
-        this.tail = newNode;
-        }
+		return this.len++;
+	}
 
+	dequeque() {
+		if (!this.head) return undefined;
 
-        return this.len++;
-    }
+		let prev = this.head;
+		this.head = this.head.next;
 
-    dequeque(){
-        if(!this.head) return undefined;
+		this.len--;
+		if (this.len == 0) this.tail = null;
 
-        let prev = this.head;
-        this.head = this.head.next;
-
-        this.len--;
-        if(this.len == 0 ) this.tail = null;
-        
-
-        return prev;
-    }
-
-
+		return prev;
+	}
 }
 
-let q = new queue;
+let q = new queue();
 q.enqueue(12);
 q.enqueue(14);
 
-q.dequeque()
-q.dequeque()
-q.dequeque()
+q.dequeque();
+q.dequeque();
+q.dequeque();
 
 console.log();
 
-//Push 
+//Push
 //shift
-
-
