7efbd116c4f4bc5389a8093a9057bde12698974f
Nishil <nishilkapadia17@gmail.com>
2020-05-13 14:07:18 +0530

    Prettier :sparkles:

--- Algorithms/Pattern/frequencyCounter.js
+++ Algorithms/Pattern/frequencyCounter.js
@@ -1,9 +1,6 @@
 //Frequency Counter
 //This pattern uses objects or sets to collect values/frequencies of values
 
-
-
-
 //Example One
 //Write an function called same, which accepts two arrays.The function should return true if every value in the second array is squared. The frequency should be same
 
@@ -15,65 +12,60 @@ same([1,2,3] , [1,9]) // False
 
 */
 
-
 //First Approach  ( Naive )
-function same(arrOne , arrTwo){
-    if(arrOne.length !== arrTwo.length){
-        return false;
-    }
+function same(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
 
-    for (let i = 0; i < arrOne.length; i++){
-        correctIndex = arrTwo.indexOf(arrOne[i] ** 2);
-        if(correctIndex == -1 ){
-            return false;
-        } 
-        arrTwo.splice(correctIndex,1);
+  for (let i = 0; i < arrOne.length; i++) {
+    correctIndex = arrTwo.indexOf(arrOne[i] ** 2);
+    if (correctIndex == -1) {
+      return false;
     }
+    arrTwo.splice(correctIndex, 1);
+  }
 
-    return true;
-};
+  return true;
+}
 
 //Time complexity : O(n^2)
 
-
 // Second Approach
 
-function sameTwo(arrOne,arrTwo){
+function sameTwo(arrOne, arrTwo) {
+  //Check if length is same
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
 
-    //Check if length is same
-    if(arrOne.length !== arrTwo.length){
-        return false;
+  //declare objects for each arrays into objects
+  let freqOne = {};
+  let freqTwo = {};
+
+  //loop arrOne and store into objects as key with freq
+  for (let val of arrOne) {
+    freqOne[val] = (freqOne[val] || 0) + 1;
+  }
+
+  //loop arrTwo and store into objects as key with freq
+  for (let val of arrTwo) {
+    freqTwo[val] = (freqTwo[val] || 0) + 1;
+  }
+
+  for (let key in freqOne) {
+    //loop check if key exists in arrtwo
+    if (!freqTwo[key ** 2]) {
+      return false;
     }
 
-     //declare objects for each arrays into objects
-    let freqOne = {};
-    let freqTwo = {};
-
-    //loop arrOne and store into objects as key with freq
-    for(let val of arrOne){
-        freqOne[val] = (freqOne[val] || 0) +  1;
+    // if exists how many?
+    if (freqTwo[key ** 2] !== freqOne[key]) {
+      return false;
     }
+  }
 
-    //loop arrTwo and store into objects as key with freq
-    for(let val of arrTwo){
-        freqTwo[val] = (freqTwo[val] || 0) +  1;
-    }
-   
-    for(let key in freqOne){
-        
-        //loop check if key exists in arrtwo
-        if(!(freqTwo[key ** 2])){
-            return false;
-        }
-
-        // if exists how many?
-        if(freqTwo[key ** 2] !== freqOne[key]){
-            return false;
-        }
-    }
-
-    return true;
-
+  return true;
 }
 
 // Time complexity : O(n);
--- DataStructures/README.md
+++ DataStructures/README.md
@@ -1,7 +1,7 @@
-# JavaScript Algorithms and Data Structures Masterclass 
+# JavaScript Algorithms and Data Structures Masterclass
 
 - [x] Singly Linked List
 - [x] Doubly Linked List
 - [x] Stacks and Queque
 - [x] Binary Search Trees
-- [ ] Trees Traversal 
+- [ ] Trees Traversal
